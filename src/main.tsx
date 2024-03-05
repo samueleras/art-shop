9ac1694833f85cb2121fa0fd3c197df47843dffd
Samuel Eras <sa.eras@gmx.de>
2024-03-05 16:00:45 +0100

    create custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/routes";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
