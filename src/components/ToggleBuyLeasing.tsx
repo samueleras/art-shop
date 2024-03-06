aba34593c6bb752efa85aad7e8f2cc6e50500438
Samuel Eras <sa.eras@gmx.de>
2024-03-06 19:46:35 +0100

    implement buy lease toggle

--- src/components/ToggleBuyLeasing.tsx
+++ src/components/ToggleBuyLeasing.tsx
@@ -1,5 +1,31 @@
+import {
+  HStack,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Text,
+} from "@chakra-ui/react";
+
 const ToggleBuyLeasing = () => {
-  return <div>ToggleBuyLeasing</div>;
+  return (
+    <HStack w="12rem" gap={5}>
+      <Text>Buy</Text>
+      <Slider
+        defaultValue={0}
+        min={0}
+        max={1}
+        step={1}
+        onChange={(value) => {}}
+      >
+        <SliderTrack bg="tomato">
+          <SliderFilledTrack bg="tomato" />
+        </SliderTrack>
+        <SliderThumb boxSize={6} />
+      </Slider>
+      <Text>Lease</Text>
+    </HStack>
+  );
 };
 
 export default ToggleBuyLeasing;
