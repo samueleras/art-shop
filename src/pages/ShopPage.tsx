985a3950c9ff89cd3747d789120b5fcc4c7412bb
Samuel Eras <sa.eras@gmx.de>
2024-03-06 18:28:20 +0100

    adjust shopPage styling and add the created components

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,82 +1,28 @@
-import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Heading,
-  Image,
-  SimpleGrid,
-  Spinner,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import useArtPieces from "../hooks/useArtPieces";
-import getImageUrl from "../services/image-url";
+import { Grid, GridItem } from "@chakra-ui/react";
+import CarGrid from "../components/CarGrid";
+import ShopControls from "../components/ShopControls";
 
 const ShopPage = () => {
-  const { data, error, isFetching } = useArtPieces();
-
-  if (error) throw new Error();
-
-  if (isFetching) return <Spinner />;
-
   return (
-    <SimpleGrid gap={3}>
-      {data?.data.map((artwork) => (
-        /*           <Box
-            key={artwork.id}
-            backgroundColor={"gray.700"}
-            p={3}
-            borderRadius={5}
-          > */
-        /*             <Box boxSize="sm">
-              <Image
-                src={getImageUrl(data.config.iiif_url, artwork.image_id)}
-                alt={artwork.title + "_image"}
-                onError={(e) => {
-                  e.currentTarget.onerror = null;
-                  e.currentTarget.src = getImageUrl(undefined, undefined);
-                }}
-              />
-            </Box>
-            <Text>Id: {artwork.id}</Text>
-            <Text>Title: {artwork.title}</Text>
-            <Text>Artist: {artwork.artist_title}</Text>
-          </Box> */
-        <Card
-          key={artwork.id}
-          direction={{ base: "column", sm: "row" }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            width={{ base: "100%", sm: "200px" }}
-            height={{ base: "100%", sm: "200px" }}
-            src={getImageUrl(data.config.iiif_url, artwork.image_id)}
-            alt={artwork.title + "_image"}
-            onError={(e) => {
-              e.currentTarget.onerror = null;
-              e.currentTarget.src = getImageUrl(undefined, undefined);
-            }}
-          />
-
-          <Stack>
-            <CardBody>
-              <Heading size="md">{artwork.title}</Heading>
-
-              <Text py="2">By {artwork.artist_title}</Text>
-            </CardBody>
-
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                Add to Cart
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
-      ))}
-    </SimpleGrid>
+    <Grid width="100%" justifyItems="center">
+      <Grid
+        width={{ base: "100%", md: "80%" }}
+        marginTop={3}
+        gridTemplateColumns={{ base: "1fr", md: "25% auto" }}
+        templateAreas={{
+          base: `"controls" "carGrid"`,
+          md: `"controls carGrid"`,
+        }}
+        gap={3}
+      >
+        <GridItem area={"controls"}>
+          <ShopControls />
+        </GridItem>
+        <GridItem area={"carGrid"}>
+          <CarGrid />
+        </GridItem>
+      </Grid>
+    </Grid>
   );
 };
 
