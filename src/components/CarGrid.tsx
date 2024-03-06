6d89a469716db2043f93e0eb8fad625af979e90b
Samuel Eras <sa.eras@gmx.de>
2024-03-06 18:26:39 +0100

    move cargrid from shoppage into separate component

--- src/components/CarGrid.tsx
+++ src/components/CarGrid.tsx
@@ -1,5 +1,23 @@
+import { Grid, Spinner } from "@chakra-ui/react";
+import useCars from "../hooks/useCars";
+import CarCard from "./CarCard";
+
 const CarGrid = () => {
-  return <div>CarGrid</div>;
+  const { data, error, isFetching } = useCars();
+
+  if (error) throw new Error();
+
+  if (isFetching) return <Spinner />;
+  return (
+    <Grid
+      gap={3}
+      gridTemplateColumns={{ base: "1fr", sm: "1fr 1fr", lg: "1fr 1fr 1fr" }}
+    >
+      {data?.data.map((car) => (
+        <CarCard car={car} imageUrl={data.config.iiif_url} key={car.id} />
+      ))}
+    </Grid>
+  );
 };
 
 export default CarGrid;
