bcc138bdbe3451123de3ad034283592a89a4bb2c
Samuel Eras <sa.eras@gmx.de>
2024-03-18 19:39:51 +0100

    update card with static data and styling

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,61 +1,69 @@
 import {
   AspectRatio,
   Button,
-  ButtonGroup,
   Card,
   CardBody,
-  CardFooter,
   Heading,
+  Icon,
   Image,
   Stack,
   Text,
 } from "@chakra-ui/react";
+import ferrarif8 from "../assets/Ferrari F8.avif";
+import imgage_placeholder from "../assets/no-image-placeholder.webp";
 import Car from "../entities/Car";
-import getImageUrl from "../services/image-url";
+import CarPropertyIconBar from "./CarPropertyIconBar";
+import { FaCartShopping } from "react-icons/fa6";
+import useCarQueryStore from "../carquery/store";
 
 interface Props {
   car: Car;
-  imageUrl: string;
 }
 
-const CarCard = ({ car, imageUrl }: Props) => {
+const CarCard = ({ car }: Props) => {
+  const {
+    carQuery: { buyOrLease },
+  } = useCarQueryStore();
+
+  const carProperties = {
+    doors: 5,
+    passengers: undefined,
+    transmission: "manual",
+    isElectric: true,
+  };
+
   return (
     <Card>
       <CardBody>
-        <Heading size="md" mb="6">
-          {car.title}
-        </Heading>
+        <Heading size="lg">Ferrari F8</Heading>
+        <Text mb="1" fontSize="rem">
+          Coupe
+        </Text>
         <AspectRatio w="100%" ratio={1}>
           <Image
             objectFit="cover"
             width="100%"
-            src={getImageUrl(imageUrl, car.image_id)}
+            src={ferrarif8}
             alt={car.title + "_image"}
             onError={(e) => {
               e.currentTarget.onerror = null;
-              e.currentTarget.src = getImageUrl(undefined, undefined);
+              e.currentTarget.src = imgage_placeholder;
             }}
           />
         </AspectRatio>
-        <Stack mt="6" spacing="1">
-          <Text>
-            {car.title} By {car.artist_title}
-          </Text>
-          <Text color="blue.600" fontSize="2xl">
-            $450
+        <Stack mt="3" spacing="1">
+          <CarPropertyIconBar car={carProperties} />
+          <Text>{"A Celebration of Excellence"}</Text>
+          <Text>{"720 HP"}</Text>
+          <Text color="blue.600" fontSize="2xl" mb="3">
+            {buyOrLease === "buy" ? "$266,661" : "$1200 leasing rate"}
           </Text>
         </Stack>
+        <Button variant="solid" colorScheme="blue" gap={3}>
+          <Icon as={FaCartShopping} color={"gray.700"} />
+          Add to cart
+        </Button>
       </CardBody>
-      <CardFooter>
-        <ButtonGroup spacing="2">
-          <Button variant="solid" colorScheme="blue">
-            Buy now
-          </Button>
-          <Button variant="ghost" colorScheme="blue">
-            Add to cart
-          </Button>
-        </ButtonGroup>
-      </CardFooter>
     </Card>
   );
 };
