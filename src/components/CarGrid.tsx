1063e20b08c1f9d71e816cd2096e2101d5d64030
Samuel Eras <sa.eras@gmx.de>
2024-06-25 18:02:04 +0200

    create temporary fixed car object for testing as there is no backend yet

--- src/components/CarGrid.tsx
+++ src/components/CarGrid.tsx
@@ -1,21 +1,50 @@
-import { Grid, Spinner } from "@chakra-ui/react";
-import useCars from "../hooks/useCars";
+import { Grid } from "@chakra-ui/react";
 import CarCard from "./CarCard";
+import Car from "../entities/Car";
+import ferrarif8 from "../assets/Ferrari F8.avif";
 
 const CarGrid = () => {
-  const { data, error, isFetching } = useCars();
+  //const { data, error, isFetching } = useCars();
 
-  if (error) throw new Error();
+  let car: Car = {
+    id: 1,
+    brand: "Ferrari",
+    model: "F8",
+    type: "coupe",
+    price: 266661,
+    leasing: 1200,
+    passengerCount: 2,
+    doorCount: 2,
+    gearbox: "automatic",
+    horsepower: 720,
+    electric: false,
+    description: "A Celebration of Excellence",
+    thumbnail: ferrarif8,
+  };
 
-  if (isFetching) return <Spinner />;
+  //if (error) throw new Error();
+
+  //if (isFetching) return <Spinner />;
   return (
     <Grid
       gap={3}
       gridTemplateColumns={{ base: "1fr", md: "1fr 1fr", lg: " 1fr 1fr 1fr" }}
     >
-      {data?.data.map((car) => (
+      {/* {data?.data.map((car) => (
         <CarCard car={car} key={car.id} />
-      ))}
+      ))} */}
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
+      <CarCard car={car} />
     </Grid>
   );
 };
