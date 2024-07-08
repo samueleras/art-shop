df21c35a20f8190fadca069a886d8aa216e4245e
Samuel Eras <sa.eras@gmx.de>
2024-07-08 18:29:25 +0200

    implement car filter modal component in shop controls with dedicated button

--- src/components/ShopControls.tsx
+++ src/components/ShopControls.tsx
@@ -1,16 +1,38 @@
-import { Box, Card, Divider, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  Divider,
+  Flex,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
+import { TbFilterSearch } from "react-icons/tb";
 import CarFilter from "./CarFilter";
+import CarFilterModal from "./CarFilterModal";
 import ToggleBuyLeasing from "./ToggleBuyLeasing";
 
 const ShopControls = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Card>
       <VStack align="start" padding={3}>
-        <ToggleBuyLeasing />
+        <Flex justifyContent="space-between" width="100%" alignItems="center">
+          <ToggleBuyLeasing />
+          <Box
+            background="none"
+            cursor="pointer"
+            onClick={onOpen}
+            display={{ sm: "block", lg: "none" }}
+          >
+            <TbFilterSearch size="1.5rem" />
+          </Box>
+        </Flex>
+
         <Box display={{ base: "none", lg: "block" }} width="100%">
           <Divider />
           <CarFilter />
         </Box>
+        <CarFilterModal isOpen={isOpen} onClose={onClose} />
       </VStack>
     </Card>
   );
