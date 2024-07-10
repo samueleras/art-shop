0a1f34d298faf604bd1617ddd07a7992c126483b
Samuel Eras <sa.eras@gmx.de>
2024-07-10 11:12:01 +0200

    add items to shopping cart with function of the zustand store differentiated by buy or lease

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,9 +1,20 @@
 import { Button, Icon } from "@chakra-ui/react";
 import { FaCartShopping } from "react-icons/fa6";
+import Car from "../entities/Car";
+import useShoppingCartStore from "../stores/shoppingCartStore";
+import useCarQueryStore from "../stores/carqueryStore";
 
-const AddToCart = () => {
+interface Props {
+  car: Car;
+}
+
+const AddToCart = ({ car }: Props) => {
+  const { addItem } = useShoppingCartStore();
+  const {
+    carQuery: { buyOrLease },
+  } = useCarQueryStore();
   return (
-    <Button variant="solid" gap={3}>
+    <Button variant="solid" gap={3} onClick={() => addItem(car.id, buyOrLease)}>
       <Icon as={FaCartShopping} color={"gray.700"} />
       Add to cart
     </Button>
