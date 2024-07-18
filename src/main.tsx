1e00d9e3b0320e48499812aa9bcd70f1bb2b2674
Samuel Eras <sa.eras@gmx.de>
2024-07-18 17:28:31 +0200

    initialize the values retrieved of the localstorage in the shoppingCartStore

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,30 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/routes";
 import theme from "./theme";
+import useShoppingCartStore from "./stores/shoppingCartStore";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+const App = () => {
+  const { initializeItems } = useShoppingCartStore();
+
+  // Initialisierung des Zustand-Stores
+  React.useEffect(() => {
+    initializeItems();
+  }, [initializeItems]);
+
+  return (
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
