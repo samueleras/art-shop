0f613d92f539c00acdf6e4ed180ae043bb1b8405
Samuel Eras <sa.eras@gmx.de>
2024-10-06 16:50:24 +0100

    update items in store before rendering shopping cart

--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -9,41 +9,44 @@ import {
   StackDivider,
   Text,
 } from "@chakra-ui/react";
-import ferrarif8png from "../assets/f8png.avif";
-import ferrarisf8 from "../assets/ferrarif8.jpg";
-import ferrarisf90 from "../assets/ferrarisf90.avif";
 import CartCheckout from "../components/CartCheckout";
 import CartCouponCode from "../components/CartCouponCode";
 import CartShipping from "../components/CartShipping";
 import ShoppingCartItemBox from "../components/ShoppingCartItemBox";
-import Car from "../entities/Car";
+import useCar from "../hooks/useCar";
 import useShoppingCartStore from "../stores/shoppingCartStore";
+import { useEffect } from "react";
 
 const ShoppingCartPage = () => {
-  const { items, overallItemCount } = useShoppingCartStore();
+  const { items, overallItemCount, updateItems } = useShoppingCartStore();
 
-  let car: Car = {
-    id: 1,
-    brand: "Ferrari",
-    model: "F8",
-    type: "coupe",
-    price: 266661,
-    leasing: 1200,
-    passengerCount: 2,
-    doorCount: 2,
-    gearbox: "automatic",
-    horsepower: 720,
-    electric: false,
-    shortDescription: "A Celebration of Excellence",
-    fuelConsumption: "7,3 l/100 km",
-    emissions: "167 g/km",
-    emissionClass: "G",
-    description:
-      "Vor etwa 20 Jahren begann Ferrari mit der Produktion von Sondermodellen, die die Performance der Serienbaureihen in eine noch höhere Dimension befördern. Viele Sondermodelle, wie der 488 Pista und der 812 Competizione, wurden Teil der Legende der Marke aus Maranello. Gleichzeitig startete das Cavallino Rampante das XX-Programm, das einer ausgewählten Gruppe erfahrener Piloten aus dem Kundenkreis die Möglichkeit bietet, sich auf der Rennstrecke als echte Testfahrer am Steuer von Modellen wie dem FXX-K EVO zu bewähren, die nicht für den Straßenverkehr zugelassen sind. Jetzt wurden die Programme zum ersten Mal zusammengeführt: im SF90 XX Spider, dem ersten Ferrari Sondermodell, das auf dem Konzept des XX-Programms basiert und daraus praktisch einen XX mit Straßenzulassung macht. Während der SF90 XX Spider das versenkbare Hardtop des SF90 Spider beibehält, auf dem er basiert, entfesselt er 30 zusätzliche PS und besitzt eine spezifische Softwarelogik und radikal neue Aero-Lösungen, wie einen festen Heckflügel.",
-    thumbnail: ferrarisf8,
-    png: ferrarif8png,
-    images: [ferrarisf8, ferrarif8png, ferrarisf90],
-  };
+  //Exchange this with bulk query and add loading state
+  for (let x of items) {
+    const { data: car, error } = useCar(x.carId);
+    if (car) {
+      updateItems(car);
+    } else {
+      throw (
+        error || new Error("An unknown error occurred while fetching the car.")
+      );
+    }
+  }
+
+  /*   useEffect(() => {
+    if (cars) {
+      updateItems(cars);  // Update the store with fetched cars after fetch finished
+    }
+  }, [cars, updateItems]); */
+
+  /*   if (isFetching) {
+    return (
+      <Box>
+        <Spinner size="xl" />
+      </Box>
+    );
+  } */
+
+  /* if (error) throw error; */
 
   return (
     <Box padding="2rem" margin="auto" width={{ base: "100%", xl: "80%" }}>
@@ -73,7 +76,6 @@ const ShoppingCartPage = () => {
               {items.length > 0 ? (
                 items.map((item) => (
                   <ShoppingCartItemBox
-                    car={car}
                     item={item}
                     key={item.carId + item.buyOrLease}
                   />
