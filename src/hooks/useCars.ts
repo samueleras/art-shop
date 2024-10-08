0dc20fa1fc0aaf9f8a8b05b3aa4a962a6a00c97f
Samuel Eras <sa.eras@gmx.de>
2024-10-08 23:22:19 +0100

    pass carquery filter options to getAll function and add it as a queryKey to assure refetching

--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import Car from "../entities/Car";
 import APIClient from "../services/apiClient";
+import { CarQuery } from "../stores/carqueryStore";
 
 const carsClient = new APIClient<Car>("/cars");
 
-const useCars = () => {
+const useCars = (carQuery: CarQuery) => {
   return useQuery<Car[]>({
-    queryKey: ["cars"],
-    queryFn: carsClient.getAll,
+    queryKey: ["cars", carQuery],
+    queryFn: () => carsClient.getAll(carQuery),
     staleTime: 10 * 1000,
     refetchOnWindowFocus: true,
   });
